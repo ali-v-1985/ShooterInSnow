f6b93c14b5100a7a0303b10ed8ff3fd849c2ee09
Ali <ali.valizadeh.h@gmail.com>
2020-10-06 23:15:00 +0200

    Add level functionalities and blueprints

---
+++ Source/ShooterInSnow/Actors/Combat/Rifle.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Rifle.h"
+
+ARifle::ARifle()
+{
+}
+
+void ARifle::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ARifle::Fire()
+{
+    // GetWorld
+}
+
+void ARifle::BeginPlay()
+{
+    Super::BeginPlay();
+}
