b0115e3249932f218e95cb3c7d42e8b17731223b
Ali <ali.valizadeh.h@gmail.com>
2020-10-06 21:50:21 +0200

    Initial Repo

---
+++ Source/ShooterInSnow/ShooterInSnowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterInSnowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERINSNOW_API AShooterInSnowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
