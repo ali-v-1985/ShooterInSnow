c1768fea7cfa514dd6ac9b90dad28d9359af878e
Ali <ali.valizadeh.h@gmail.com>
2020-10-11 13:25:28 +0200

    Clear blackboard value by adding a custom behavior tree task for that

--- Source/ShooterInSnow/ShooterInSnow.Build.cs
+++ Source/ShooterInSnow/ShooterInSnow.Build.cs
@@ -8,7 +8,7 @@ public class ShooterInSnow : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
